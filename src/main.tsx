699e9cb0d75553cf953efb51c9d46c0efa957d66
petitebetterave <anabasenft@gmail.com>
2024-02-14 06:11:56 +0100

    added panda, clerk and env

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const CLERK_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!CLERK_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={CLERK_KEY}>
+      <App />
+    </ClerkProvider>
   </React.StrictMode>,
 )
